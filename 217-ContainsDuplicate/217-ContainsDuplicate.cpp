ef88be6780191301cc6c36af75525073b015395b
sequenc <skip2025nws@gmail.com>
2026-08-16 13:55:29 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/16/2026, 1:55:31 PM
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size();
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n-1;i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
